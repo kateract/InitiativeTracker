a87a4229061c1c55c8899ba1c40d887c3df2b6c4
Kateract <kateract@yahoo.com>
2014-01-07 18:18:28 -0600

    Initial Commit

---
+++ InitiativeObjects/Character.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace InitiativeObjects
+{
+    class Character
+    {
+        public string name;
+        public int maxHP;
+        public int currentHP;
+        public int AC;
+        public int InitiativeMod;
+        
+        public Character(string name, int hp, int ac, int init)
+        {
+            this.name = name;
+            this.currentHP = hp;
+            this.maxHP = hp;
+            this.AC = ac;
+            this.InitiativeMod = init;
+        }
+
+        public override string ToString()
+        {
+            return name;
+        }
+    }
+
+    class Combat
+    {
+        public int round;
+        public Queue<Character> InitiativeList;
+        public List<Character> HeldList;
+        public List<Character> ReadiedList;
+        public List<Character> PlayerCharacters;
+        public List<Character> NonPlayerCharacters;
+        public List<Character> Enemies;
+
+        public Combat()
+        {
+            InitiativeList = new Queue<Character>();
+            HeldList = new List<Character>();
+            ReadiedList = new List<Character>();
+            PlayerCharacters = new List<Character>();
+            NonPlayerCharacters = new List<Character>();
+            Enemies = new List<Character>();
+        }
+    }
+}
---
+++ InitiativeTrackerConsole/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Data;
+using System.Text.RegularExpressions;
+
+
+namespace InitiativeTrackerConsole
+{
+    class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+            FileInfo players = new FileInfo("pc.txt");
+            StreamReader sr = new StreamReader(players.OpenRead());
+            Combat c = new Combat();
+
+            string line;
+            string[] parts;
+            do
+            {
+                line = sr.ReadLine().Trim();
+                if (line.Length > 0)
+                {
+                    parts = line.Split(',');
+                    Console.WriteLine(line);
+                    c.PlayerCharacters.Add(new Character(parts[0], int.Parse(parts[1]), int.Parse(parts[2]), int.Parse(parts[3])));
+                }
+            }
+            while (!sr.EndOfStream);
+
+            foreach (Character item in c.PlayerCharacters)
+            {
+                c.ToString();
+            }
+
+        }
+     
+    }
+}
